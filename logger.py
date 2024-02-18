8420c6105140a0b88ed9a1f262ef25159610fd2a
clomez <elkku.nyman@gmail.com>
2024-02-18 20:17:25 +0200

    cleanup

--- logger.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def hourStamp():
-    current_dateTime = datetime.now()
-    return f"{current_dateTime.hour}-{current_dateTime.minute}"
-
-def printToLog(msg):
-    with open('log/log.txt', 'a') as f:
-        f.write(f"{hourStamp()} -- {msg}\n")
